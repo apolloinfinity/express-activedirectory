f2f68ae3eef01e6e8f85d7cd1666e6ef76a5e6f5
Javier Ramirez <apollo.infinity88@gmail.com>
2020-06-29 22:47:44 -0700

    Updated Auth middleware to use ES6 syntax, experimental checkGroup function was added and it works

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	// Checks to see if the user is authenticated
 	// If not, it wil redirect to the login screen
-	ensureAuth: function(req, res, next) {
+	ensureAuth: (req, res, next) => {
 		if (req.isAuthenticated()) {
 			return next();
 		} else {
@@ -11,11 +11,27 @@ module.exports = {
 	// Once a user is logged in and authenticated,
 	// a person can't go back to the login in page
 	// and gets redirected back to the dashboard
-	forwardAuth: function(req, res, next) {
+	forwardAuth: (req, res, next) => {
 		if (!req.isAuthenticated()) {
 			return next();
 		} else {
 			res.redirect('/dashboard');
 		}
+	},
+
+	// TODO: will need to add users are allowed to see what once I expand the project more.
+	checkGroup: (req, res, next) => {
+		let groups = req.user.groups;
+		for (group of groups) {
+			if (group.cn === 'Technicians') {
+				res
+					.status(401)
+					.render('401', {
+						pageTitle: 'Unauthorized',
+					})
+					.end();
+			}
+		}
+		next();
 	},
 };
