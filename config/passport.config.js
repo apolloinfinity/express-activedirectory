a21f456b31fa98b14af434f568447da3ee16320b
Javier Ramirez <apollo.infinity88@gmail.com>
2020-07-02 23:27:33 -0700

    Renamed passport.js to passport.config, profile has a custom property to pass groups that a user is in to req.user

---
+++ config/passport.config.js
@@ -0,0 +1,37 @@
+const ActiveDirectoryStrategy = require('passport-activedirectory');
+
+const AD = require('./ad.config');
+const ad = AD;
+
+module.exports = (passport) => {
+	passport.serializeUser((user, done) => {
+		console.log('userStrategy -- serialized:', user);
+		done(null, user);
+	});
+
+	passport.deserializeUser((user, done) => {
+		console.log('userStrategy -- deserializeUser', user);
+		done(null, user);
+	});
+
+	passport.use(
+		new ActiveDirectoryStrategy(
+			{
+				integrated: false,
+				usernameField: 'email',
+				ldap: ad,
+			},
+			function(profile, ad, done) {
+				ad.getGroupMembershipForUser(profile._json.dn, function(
+					err,
+					groups
+				) {
+					// console.log(groups);
+					if (err) return done(err);
+					profile.groups = groups;
+					return done(null, profile);
+				});
+			}
+		)
+	);
+};
