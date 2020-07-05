18b67a2beae44f6b378acae86e396e83b465a00a
Javier Ramirez <apollo.infinity88@gmail.com>
2020-07-05 12:18:07 -0700

    Added express-handlebars, morgan, flash, set up the view engine with hbs options, and set flash with error types

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const dotenv = require('dotenv');
+const handlebars = require('express-handlebars');
+const logger = require('morgan');
+const flash = require('connect-flash');
 
 const connectDB = require('./config/database.config');
 
@@ -15,9 +18,22 @@ const app = express();
 
 require('./config/passport.config')(passport);
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+app.set('view engine', 'hbs');
+app.engine(
+	'hbs',
+	handlebars({
+		extname: 'hbs',
+		layoutsDir: __dirname + '/views/layouts/',
+		defaultLayout: 'layout',
+		partialsDir: __dirname + '/views/partials/',
+	})
+);
 
+app.use(express.static('public'));
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -33,6 +49,16 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(flash());
+
+// Global Variables for flash messages
+app.use((req, res, next) => {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
 
 app.use('/users', require('./routes/user.routes'));
 app.use('/', require('./routes/index.routes.js'));
