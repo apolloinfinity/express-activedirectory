d5a0d380248cbdb88b3900993c2cee05a4394594
Javier Ramirez <apollo.infinity88@gmail.com>
2020-07-05 12:22:58 -0700

    checkGroup middleware is put on the /stuffs route for testing, might change name to checkPermissions

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,7 +5,6 @@ const { dashboard, stuffs } = require('../controllers/index.controller');
 const { ensureAuth, checkGroup } = require('../middleware/auth');
 
 router.use(ensureAuth);
-// router.use(checkGroup);
 
 router.get('/dashboard', dashboard);
 router.get('/stuffs', checkGroup, stuffs);
