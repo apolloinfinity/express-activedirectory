633b341f49e77383a3263c30b450fdb378d4ede7
Javier Ramirez <apollo.infinity88@gmail.com>
2020-07-14 07:13:56 -0700

    Added passport authentication to routes file. Managed to stop the app from crashing if the wrong password is typed, need to fix flash message

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,29 @@
 const { Router } = require('express');
 const router = Router();
+const passport = require('passport');
 
 const { forwardAuth } = require('../middleware/auth');
 const { loginPage, login, logout } = require('../controllers/user.controller');
 
+const opts = {
+	failWithError: true,
+	// failureRedirect: '/user',
+	successRedirect: '/dashboard',
+	failureFlash: true,
+};
+
 router.get('/', forwardAuth, loginPage);
-router.post('/login', login);
+router.post(
+	'/login',
+	passport.authenticate('ActiveDirectory', opts),
+	(req, res) => {
+		res.json(req.user);
+	},
+	(err, req, res, next) => {
+		if (err) return res.status(401).render('login');
+		res.flash('Wrong password');
+	}
+);
 router.get('/logout', logout);
 
 module.exports = router;
