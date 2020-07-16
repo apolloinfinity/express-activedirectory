1966864765ea57d182f200cf8b8e9085aef45bd3
Javier Ramirez <apollo.infinity88@gmail.com>
2020-07-15 20:02:45 -0700

    Updated stuffs to clients, a route that needs authorization to see, used mock data uploaded to a vmware hosted mongodb DB

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,4 +1,6 @@
-exports.dashboard = (req, res) => {
+const Client = require('../models/Clients.models');
+
+exports.dashboard = async (req, res) => {
 	res.render('home', {
 		user: req.user,
 		title: `Home`,
@@ -6,9 +8,17 @@ exports.dashboard = (req, res) => {
 	});
 };
 
-exports.stuffs = (req, res) => {
-	res.render('stuffs', {
-		title: 'Stuffs',
-		loggedIn: true,
-	});
+exports.clients = async (req, res) => {
+	const clients = await Client.find({}).lean();
+
+	try {
+		// clients.map((client) => console.log(client));
+		res.render('clients', {
+			title: 'Clients',
+			loggedIn: true,
+			clients: clients,
+		});
+	} catch (error) {
+		throw error;
+	}
 };
