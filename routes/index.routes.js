28dc4cfc39b914bb985b22aea3d8a32b9f2c1d6e
Javier Ramirez <apollo.infinity88@gmail.com>
2020-07-29 09:58:34 -0700

    Bringing in search filter from it's own file, route for specific client is working

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,19 @@
 const { Router } = require('express');
 const router = Router();
 
-const { dashboard, clients } = require('../controllers/index.controller');
+const {
+	dashboard,
+	clients,
+	clientProfile,
+} = require('../controllers/index.controller');
+const { search } = require('../controllers/search.controller');
 const { ensureAuth, checkGroup } = require('../middleware/auth');
 
 router.use(ensureAuth);
 
 router.get('/dashboard', dashboard);
 router.get('/clients', checkGroup, clients);
-router.get('/clients/:id');
+router.get('/clients/:id', clientProfile);
+router.get('/search-client/', search);
 
 module.exports = router;
