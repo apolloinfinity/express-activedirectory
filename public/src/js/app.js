008e257f01e168dbade6becd2d7bfffa0a67a57b
Javier Ramirez <apollo.infinity88@gmail.com>
2020-07-31 13:41:02 -0700

    Updated the autocomplete to use jquery ui, and make a req for a specific client, once found

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -19,18 +19,60 @@ $(function() {
 				type: 'GET',
 				data: req,
 				success: function(data) {
-					console.log(data);
+					res(data);
 				},
 				error: function(err) {
 					console.error(err.stats);
 				},
 			});
 		},
-		minLength: 1,
+		minLength: 1, // Chane to a minimum of 2 or 3 later
 		select: function(event, ui) {
 			if (ui.item) {
-				$('#search').text(ui.item.label);
+				$('#search').text(
+					`${ui.item.label}  ${ui.item.label2} ${ui.item.company}`
+				);
 			}
 		},
+	});
+});
+
+$('#btn-search').click(function(e) {
+	const value = $('#search').val();
+	let wholeName = value.split(' ');
+	let id = wholeName[0];
+	$.get('/search', { id: id }, (data, status) => {
+		console.log(data);
+		$('.results').addClass('card shadow-sm').html(`
+		<ul class="list-group list-group-flush">
+            
+            <li class="list-group-item d-flex justify-content-around align-items-center">
+                <div class="text-center">
+                    <p><strong>Client ID:</strong></p>
+                    <p onClick="newTab()" class="workOrder" id="workOrderLink"><i class="fas fa-binoculars"></i> ${data.id}</p>
+                </div>
+                <div class="text-center">
+                    <p><strong>Name:</strong></p>
+                    <p>${data.first_name} ${data.last_name}</p>
+                </div>
+                <div class="text-center">
+                    <p><strong>Company:</strong></p>
+                    <p>${data.company_name}</p>
+				</div>
+				<div class="text-center">
+                    <p><strong>Department:</strong></p>
+                    <p>${data.department}</p>
+				</div>
+				<div class="text-center">
+                    <p><strong>Support Tickets Open:</strong></p>
+                    <p>${data.support_tickets
+						? data.support_tickets.length
+						: 0}</p>
+                </div>
+                
+                
+            </li>
+        </ul>
+		`);
 	});
 });
